b30776e396173b9443b58e5368de97318f071af1
Betsy Louis <blouis1@my.westga.edu>
2016-01-26 12:03:03 -0500

    Created third activity with UI to display 2 messages.

--- app/src/main/java/com/example/betsy/betsylouisactivities/DisplayBothActivity.java
+++ app/src/main/java/com/example/betsy/betsylouisactivities/DisplayBothActivity.java
@@ -1,23 +1,18 @@
 package com.example.betsy.betsylouisactivities;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
 
-public class DisplayMessageActivity extends AppCompatActivity {
-
-    public final static String NEW_MESSAGE = "com.example.betsy.betsylouisactivities.NEW_MESSAGE";
+public class DisplayBothActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_message);
+        setContentView(R.layout.activity_display_both);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -29,22 +24,7 @@ public class DisplayMessageActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        TextView textView = (TextView) findViewById(R.id.textView_message);
-        textView.setText(message);
-
     }
 
-    /** Called when the user clicks the Next button */
-    public void nextMessage(View view) {
-        Intent intent = new Intent(this, DisplayBothActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message2);
-        String message = editText.getText().toString();
-        intent.putExtra(NEW_MESSAGE, message);
-        startActivity(intent);
-    }
 }
